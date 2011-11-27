ca758559fae05c4843ba7b3af7f4e85d8562f6f5
Simon Gomizelj <simongmzlj@gmail.com>
2011-11-26 21:18:33 -0500

    Suppress some warning for now.

--- buffer.c
+++ buffer.c
@@ -168,61 +168,61 @@ void termbuf_putchar(void *vb, char c)
 void termbuf_puttab(void *vb)
 {
 	assert(vb);
-	termbuf_t *b = vb;
+	/* termbuf_t *b = vb; */
 
-	int spaces = tabwidth - b->cursor.x % tabwidth;
+	/* int spaces = tabwidth - b->cursor.x % tabwidth; */
 }
 
 void termbuf_putcr(void *vb)
 {
 	assert(vb);
-	termbuf_t *b = vb;
+	/* termbuf_t *b = vb; */
 }
 
 void termbuf_putnl(void *vb)
 {
 	assert(vb);
-	termbuf_t *b = vb;
+	/* termbuf_t *b = vb; */
 }
 
 void termbuf_insertblank(void *vb, int n)
 {
 	assert(vb);
-	termbuf_t *b = vb;
+	/* termbuf_t *b = vb; */
 }
 
 void termbuf_insertblankline(void *vb, int n)
 {
 	assert(vb);
-	termbuf_t *b = vb;
+	/* termbuf_t *b = vb; */
 }
 
 void termbuf_deletechar(void *vb, int n)
 {
 	assert(vb);
-	termbuf_t *b = vb;
+	/* termbuf_t *b = vb; */
 }
 
 void termbuf_moveto(void *vb, int x, int y)
 {
 	assert(vb);
-	termbuf_t *b = vb;
+	/* termbuf_t *b = vb; */
 }
 
 void termbuf_clearregion(void *vb, int x1, int y1, int x2, int y2)
 {
 	assert(vb);
-	termbuf_t *b = vb;
+	/* termbuf_t *b = vb; */
 }
 
 void termbuf_savecursor(void *vb)
 {
 	assert(vb);
-	termbuf_t *b = vb;
+	/* termbuf_t *b = vb; */
 }
 
 void termbuf_loadcursor(void *vb)
 {
 	assert(vb);
-	termbuf_t *b = vb;
+	/* termbuf_t *b = vb; */
 }
